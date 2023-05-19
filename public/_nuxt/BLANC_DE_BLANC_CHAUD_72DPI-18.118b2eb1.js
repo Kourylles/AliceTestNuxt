90938e17878b26e07d1125470edae91ac636153c
kourylles <sitemarchand@gmail.com>
2023-05-19 18:40:18 +0200

    Mofification de la clef basUrl du fichier nuxtconfig

---
+++ public/_nuxt/BLANC_DE_BLANC_CHAUD_72DPI-18.118b2eb1.js
@@ -0,0 +1,1 @@
+import"./entry.3c01c47b.js";const r=""+new URL("Logo_Alice.f4a14557.png",import.meta.url).href,t=""+new URL("BLANC_DE_BLANC_CHAUD_72DPI-18.7b5f1346.jpg",import.meta.url).href;export{r as _,t as a};
---
+++ public/_nuxt/config.c18f3d7e.js
@@ -0,0 +1,1 @@
+import{B as p,q as o,C as i}from"./entry.3c01c47b.js";const f={},t=p(f);function s(){const n=o();return n._appConfig||(n._appConfig=i(t)),n._appConfig}export{s as u};
